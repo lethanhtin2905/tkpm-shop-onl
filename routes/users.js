515f8a389d79cf72d5c5214adca872949de95e76
lethanhtin2905 <57804580+lethanhtin2905@users.noreply.github.com>
2021-06-08 21:29:44 +0700

    Add func checkout

--- routes/users.js
+++ routes/users.js
@@ -5,23 +5,22 @@ const { ensureAuthenticated } = require('../config/auth');
 
 // Register Page
 router.get('/register', accountControllers.registerPage);
-
 // Register Handle
 router.post('/register', accountControllers.registerHandle);
 
 // Profile
 router.get('/profile', ensureAuthenticated, accountControllers.profile);
-
 // Update Profile
 router.post('/update-profile', accountControllers.updateProfile)
 
 // Login Page
 router.get('/login', accountControllers.loginPage);
-
 // Login Handle
 router.post('/login', accountControllers.loginHandle);
 
 // Checkout Page
 router.get('/checkout', ensureAuthenticated, accountControllers.checkoutPage);
+// Checkout Handle
+router.post('/checkout', orderControllers.checkoutHandle);
 
 module.exports = router;
